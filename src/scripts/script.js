0d14cf4e490a1c7dbfbb3be1b0164a87d3b10707
Nivea <niveamartins@dcc.ufrj.br>
2021-07-28 23:25:47 -0300

    verifying if the number is even and showin card selected

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,6 @@
 let quantityOfCards = Number(prompt("Com quantas cartas você deseja jogar?"));
-
-while (
-  quantityOfCards < 4 ||
-  quantityOfCards > 14 ||
-  !Number.isInteger(quantityOfCards)
-) {
-  quantityOfCards = Number(
-    prompt("Resposta inválida :( \n Com quantas cartas você deseja jogar?")
-  );
-}
-
+let selectedCards = [];
+let gameCards = [];
 let availableCards = [
   "bobrossparrot.gif",
   "explodyparrot.gif",
@@ -20,24 +11,50 @@ let availableCards = [
   "unicornparrot.gif",
 ];
 
-let gameCards = []
+while (
+  quantityOfCards < 4 ||
+  quantityOfCards > 14 ||
+  !Number.isInteger(quantityOfCards) ||
+  !(quantityOfCards%2 === 0)
+) {
+  quantityOfCards = Number(
+    prompt("Resposta inválida :( \n Com quantas cartas você deseja jogar?")
+  );
+}
 
 function shuffleCards() {
-    availableCards.sort(comparator)
+  availableCards.sort(comparator);
+  for (let card = 0; card < (quantityOfCards/2); card++) {
+    gameCards.push(availableCards[card]);
+    gameCards.push(availableCards[card]);
+  }
+  gameCards.sort(comparator);
 
-    for (let card = 0; card < quantityOfCards; card++) {
-        gameCards.push(availableCards[card])
-        gameCards.push(availableCards[card])
-    }
+  showingCards();
+}
 
-    gameCards.sort(comparator)
+function showingCards() {
+  let cardString = "";
+  for (let card = 0; card < gameCards.length; card++) {
+    cardString =
+      cardString +
+      '<div class="card" onclick="selectCard(this)">\n<img class="front" src="./src/img/front.png" />\n<img class="back hidden" src="./src/img/' + gameCards[card] + '"/>\n</div>\n';
+  }
+  document.querySelector(".cards").innerHTML = cardString;
+}
 
-    alert(gameCards)
+function selectCard(card) {
+    frontCard = card.querySelector(".front")
+    backCard = card.querySelector(".back")
+
+    frontCard.classList.add("hidden")
+    backCard.classList.remove("hidden")
+
+
 }
 
 function comparator() {
   return Math.random() - 0.5;
 }
 
-
-shuffleCards()
+shuffleCards();
